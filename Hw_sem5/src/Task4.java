c38b431d01d10c2603dbce88c1c6c88098d22158
L0GIN0UT <ivan.login.02@mail.ru>
2023-09-17 15:30:22 +0300

    Upload idea for 4 task

--- Hw_sem5/src/Task4.java
+++ Hw_sem5/src/Task4.java
@@ -2,7 +2,7 @@ public class Task4 {
     // *На шахматной доске расставить
     // 8 ферзей так, чтобы они не били друг друга.
 
-    private static boolean canPlaseQueen(int[][] chessField, int col) {
+    private static boolean canPlaceQueen(int[][] chessField, int col) {
         if (col >= size) {
             return true;
         }
@@ -12,14 +12,13 @@ public class Task4 {
                 // размещаем
                 chessField[i][col] = 1;
                 // проверяем
-                if (canPlaseQueen(chessField, col + 1)) {
+                if (canPlaceQueen(chessField, col + 1)) {
                     return true;
                 }
                 // возвращаем если нет
                 chessField[i][col] = 0;
             }
         }
-
         return false;
     }
 
@@ -102,10 +101,112 @@ public class Task4 {
 
     public static void main(String[] args) {
         int[][] chessField = new int[size][size];
-        if (canPlaseQueen(chessField, 0)) {
+        if (canPlaceQueen(chessField, 0)) {
             printBoard(chessField);
         } else {
             System.out.println("Решение не найдено.");
         }
     }
+
+// Идея для вывода всех решений(слишком много условий не до конца разобрался)
+//    public static int[] replace(int value){
+//        int[]temp = new int[8];
+//        for (int i = 0; i < temp.length; i ++) {
+//            if (value > 7) {
+//                value = 0;
+//            }
+//            temp[i]=value;
+//            value++;
+//        }
+//        return temp;
+//    }
+
+//
+//
+//    private static boolean canPlaseQueen(int[][] chessField, int row, int col) {
+//
+//        int[] rows = replace(row);
+//        int[] cols = replace(col);
+//
+//        if (col >= size) {
+//        return true;
+//        }
+//
+//
+//        for (int i = 0; i < rows.length; i++) {
+//            for (int j = 0; j < cols.length; j++){
+//                if (dontCross(chessField, i, j)) {
+//                    // размещаем
+//                    chessField[rows[i]][cols[j]] = 1;
+//                    // проверяем
+//                    if (canPlaseQueen(chessField, row,col)) {
+//                        return true;
+//                    }
+//                    // возвращаем если нет
+//                    chessField[rows[i]][cols[j]] = 0;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+//
+//    private static boolean dontCross(int[][] chessField, int row, int col) {
+//        // Проверка по горизонтали
+//        int[] rows = replace(row);
+//        int[] cols = replace(col);
+//
+//        for (int i = 0; i < rows.length; i++) {
+//            for(int j = 0; j < cols.length; j++) {
+//                if (chessField[rows[i]][cols[j]] == 1) {
+//                    return false;
+//                }
+//            }
+//        }
+//
+//        // Проверка по диагонали ↖
+//        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+//            if (chessField[rows[i]][cols[j]] == 1) {
+//                return false;
+//            }
+//        }
+//
+//        // Проверка по диагонали ↗
+//        for (int i = row, j = col; i < size && j >= 0; i++, j--) {
+//            if (chessField[rows[i]][cols[j]] == 1) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
+//
+//    private static void printBoard(int[][] board) {
+//
+//        System.out.println("_________________");
+//        for (int i = 0; i < size; i++) {
+//            for (int j = 0; j < size; j++) {
+//                System.out.print("|");
+//                System.out.print(board[i][j]);
+//            }
+//            System.out.print("|");
+//            System.out.println();
+//        }
+//        System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾");
+//
+//    }
+//    static final int size = 8;
+//
+//    public static void main(String[] args) {
+//        int[][] chessField = new int[size][size];
+//        for(int i = 0; i < size; i++) {
+//            for(int j = 0; j< size;j++){
+//                if (canPlaseQueen(chessField, i,j)) {
+//                    System.out.println("Решение для пары: X - " + i + " Y - " + j);
+//                    printBoard(chessField);
+//                } else {
+//                    System.out.println("Решение не найдено.");
+//                }
+//            }
+//        }
+//    }
 }
